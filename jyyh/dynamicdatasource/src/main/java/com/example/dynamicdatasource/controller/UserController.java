b4d7a45efd9e2d726587b5370df7b5e32311484e
zhangjie <420964597@qq.com>
2019-06-12 17:33:37 +0800

    提交

--- jyyh/dynamicdatasource/src/main/java/com/example/dynamicdatasource/controller/UserController.java
+++ jyyh/dynamicdatasource/src/main/java/com/example/dynamicdatasource/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.dynamicdatasource.controller;
 
+import com.example.dynamicdatasource.dataSourceTest.DataSourceContextHolder;
 import com.example.dynamicdatasource.dataobject.UserDO;
 import com.example.dynamicdatasource.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -20,6 +21,12 @@ public class UserController {
     @RequestMapping("/save")
     @ResponseBody
     public String save(UserDO user) {
+        // 切换数据源，之后的请求会去连接池中取数据库连接，连接池不与线程绑定
+         /*
+          若动态设置数据源，每设置下数据源则会重新创建连接池，
+          所以动态设置数据源不应该在每个方法里，这样每次请求线程进入后，都不是从连接池中获取的数据连接
+        */
+        DataSourceContextHolder.setDB("test");
         userService.add(user);
         return "ok";
     }
--- jyyh/dynamicdatasource/src/main/java/com/example/dynamicdatasource/service/UserServiceImpl.java
+++ jyyh/dynamicdatasource/src/main/java/com/example/dynamicdatasource/service/UserServiceImpl.java
@@ -16,14 +16,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void add(UserDO user) {
-
-
-       /* 切换数据源，之后的请求会去连接池中取数据库连接，连接池不与线程绑定。
-          若动态设置数据源，每设置下数据源则会重新创建连接池，
-          所以动态设置数据源不应该在每个方法里，这样每次请求线程进入后，都不是从连接池中获取的数据连接
-        */
-       DataSourceContextHolder.setDB("test");
-
         // 操作数据库
         userDOMapper.insertSelective(user);
         UserDO userDO = userDOMapper.selectByPrimaryKey(1);
